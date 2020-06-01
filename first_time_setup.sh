cf4d0cc823932d6719b230c83cf7df14ed7bcd42
Dylan Irlbeck <dylanirlbeck@gmail.com>
2020-06-01 10:53:59 -0500

    Adding first time setup scripts (#10)

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -1,2 +1,21 @@
+#!/usr/bin/env bash
+
+echo "Read through this file first. Hit ctrl+c now."
+read -n 1
+
+## TODO investigate using a Brewfile
+# brew bundle
+
 echo 'Symlink setup'
-source scripts/symlink.sh
+source ./install
+
+echo 'Package and app downloads'
+source scripts/bootstrap.sh
+
+echo 'MacOS Setup'
+source scripts/setup_mac.sh
+
+echo 'Dock Setup'
+source scripts/dock.sh
+
+echo "Done. Reload your terminal!"
---
+++ scripts/dock.sh
@@ -0,0 +1,45 @@
+#1/bin/bash
+
+# Remove Dock items
+echo -b 'Setting defaults for Dock...'
+
+main() {
+  if type dockutil &>/dev/null; then
+
+      dockutil --no-restart \
+          --remove 'System Preferences' \
+          --remove 'App Store' \
+          --remove 'Maps' \
+          --remove 'Notes' \
+          --remove 'Photos' \
+          --remove 'Messages' \
+          --remove 'Contacts' \
+          --remove 'Calendar' \
+          --remove 'FaceTime' \
+          --remove 'Feedback Assistant' \
+          --remove 'Siri' \
+          --remove 'Launchpad' \
+          --remove 'Numbers' \
+          --remove 'Pages' \
+          --remove 'Keynote' \
+          --remove 'iBooks' \
+          --remove 'Mail' \
+          --remove 'Music' \
+          --remove 'Podcasts' \
+          --remove 'TV' \
+          --remove 'News' \
+          --add /Applications/kitty.app \
+          -- add /Applications/Visual Studio Code.app \
+          --add /Applications/Notion.app \
+          --add /Applications/Slack.app \
+          &>/dev/null
+
+      killall cprefsd &>/dev/null
+      killall -HUP Dock &>/dev/null
+
+  else
+    echo 'ERROR: dockutil not found'
+  fi
+}
+
+main
